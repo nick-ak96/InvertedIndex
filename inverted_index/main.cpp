262c0dd809896c61a9366ca670fea177aa10de95
Mikita Akulich <nikita.akulich96@gmail.com>
2019-06-01 17:08:00 +0200

    Initial Commit

---
+++ inverted_index/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  inverted_index
+//
+//  Created by Nikita Akulich on 01/06/2019.
+//  Copyright © 2019 Mikita Akulich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
